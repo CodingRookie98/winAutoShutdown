530c1b005bea0ff2f1639f2f20f8cea9cef4740b
CodingRookie <520whjoker@gmail.com>
2024-03-02 13:38:43 +0800

    update main window title and remove useless code

--- src/ui/mainWindow.cpp
+++ src/ui/mainWindow.cpp
@@ -33,6 +33,7 @@ MainWindow::~MainWindow() {
 }
 
 void MainWindow::init() {
+    this->setWindowTitle(tr("Windows Auto Shutdown By CodingRookie"));
     trayIcon_->setContextMenu(menuTrayIcon);
     trayIcon_->show();
 }
@@ -82,15 +83,4 @@ void MainWindow::closeEvent(QCloseEvent *event) {
         QApplication::exit(0);
     });
     messageBox.exec();
-}
-
-void MainWindow::changeEvent(QEvent *event) {
-    //    if (event->type() == QEvent::WindowStateChange) {
-    //        if (isMinimized()) {
-    //            // 窗口最小化时，将窗口隐藏到系统托盘
-    //            hide();
-    //            event->ignore();
-    //        }
-    //    }
-    //    QMainWindow::changeEvent(event);
 }
