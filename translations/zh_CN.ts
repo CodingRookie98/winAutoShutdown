dd75e536d9ab2f744bc8c6d39b9d103d626bcc45
CodingRookie98 <520whjoker@gmail.com>
2025-02-11 22:54:56 +0800

    Add support for both Chinese and English languages.

---
+++ translations/zh_CN.ts
@@ -0,0 +1,160 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="zh_CN">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../src/ui/aboutDialog.ui" line="14"/>
+        <source>AboutDialog</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/ui/mainWindow.ui" line="64"/>
+        <source>帮助</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainWindow.ui" line="70"/>
+        <source>语言</source>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainWindow.ui" line="81"/>
+        <source>关于</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainWindow.ui" line="86"/>
+        <source>English</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainWindow.ui" line="91"/>
+        <source>简体中文</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainWindow.cpp" line="44"/>
+        <source>Windows Auto Shutdown By CodingRookie</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/mainWindow.cpp" line="111"/>
+        <source>Restart the program to apply the language change?</source>
+        <translation>是否重启程序以应用语言更改？</translation>
+    </message>
+</context>
+<context>
+    <name>MenuTrayIcon</name>
+    <message>
+        <location filename="../src/ui/menuTrayIcon.cpp" line="16"/>
+        <source>退出</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/menuTrayIcon.cpp" line="17"/>
+        <source>主界面</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>OptionWidget</name>
+    <message>
+        <location filename="../src/ui/optionWidget.ui" line="20"/>
+        <source>OptionWidget</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.ui" line="43"/>
+        <location filename="../src/ui/optionWidget.cpp" line="73"/>
+        <source>任务类型</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.ui" line="60"/>
+        <location filename="../src/ui/optionWidget.cpp" line="73"/>
+        <source>时间类型</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.ui" line="95"/>
+        <location filename="../src/ui/optionWidget.cpp" line="61"/>
+        <source>定时</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.ui" line="105"/>
+        <location filename="../src/ui/optionWidget.cpp" line="62"/>
+        <source>倒计时</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.ui" line="136"/>
+        <source>添加任务</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.ui" line="159"/>
+        <source>删除任务</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.cpp" line="55"/>
+        <source>锁定</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.cpp" line="56"/>
+        <source>睡眠</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.cpp" line="57"/>
+        <source>休眠</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.cpp" line="58"/>
+        <source>关机</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.cpp" line="59"/>
+        <source>重启</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.cpp" line="64"/>
+        <source>s/秒</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.cpp" line="65"/>
+        <source>m/分</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.cpp" line="66"/>
+        <source>h/时</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.cpp" line="67"/>
+        <source>d/天</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.cpp" line="73"/>
+        <source>执行时间</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/optionWidget.cpp" line="157"/>
+        <source>请选择一个任务</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
